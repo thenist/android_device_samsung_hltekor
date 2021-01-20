aebbbc815b7626f495d50d50e993da36e4f2b10b
thenist <p717394@gmail.com>
2021-01-20 20:49:15 +0900

    Update and rename lineage_hltekor.mk to aosp_hltekor.mk

--- aosp_hltekor.mk
+++ aosp_hltekor.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/hltekor/full_hlte.mk)
 
 PRODUCT_DEVICE := hltekor
-PRODUCT_NAME := lineage_hltekor
+PRODUCT_NAME := aosp_hltekor
